fdec39326454933bc3d19c3e2925fb60c1131893
yrk1n <yarkin.kanli@bilgiedu.net>
2023-04-03 15:03:40 +0300

    first commit

---
+++ BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuberBreakfast.Contracts.Breakfast
+{
+    public class BreakfastResponse
+    {
+        
+    }
+}
